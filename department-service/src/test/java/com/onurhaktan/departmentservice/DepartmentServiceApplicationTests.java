363077c6bbc1a80be7dca084af69956907f6348e
Onur Haktan <onurhaktan243@gmail.com>
2024-03-05 17:00:17 +0300

    setup and import microservices

---
+++ department-service/src/test/java/com/onurhaktan/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onurhaktan.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
